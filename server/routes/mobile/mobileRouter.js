506bf34f344c0ed1bfe2b81d6f0ef86362cc082d
noodle <noodle_292@yahoo.com>
2014-12-20 15:34:10 -0800

    Implement server to send fake event data.  Closes #10

--- server/routes/mobile/mobileRouter.js
+++ server/routes/mobile/mobileRouter.js
@@ -1,7 +1,20 @@
 //Require endpoints from folders
 
 var mobileRouter = function(req, res) {
-  res.send(200);
+  res.json([
+    {
+      eventName: 'sampleEvent',
+      regions: [{
+        regionName: 'sampleRegion',
+        coordinates: [
+          [37.787050, -122.409188],
+          [37.785901, -122.411334],
+          [37.782291, -122.408976],
+          [37.785505, -122.405334]
+        ]
+      }]
+    }
+  ]);
 }
 
 module.exports = mobileRouter;
