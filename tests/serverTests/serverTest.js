4ef320226d4fe9dee393c8f4aa5942ea4f4c3bbe
noodle <noodle_292@yahoo.com>
2014-12-29 15:39:19 -0800

    (feat) Add basis for server integration tests

--- tests/serverTests/serverTest.js
+++ tests/serverTests/serverTest.js
@@ -1,4 +1,29 @@
-describe('something', function() {
-  it('should pass!', function() {
+var request = require('supertest');
+var app = require('./../../server/server.js');
+var assert = require('chai').assert;
+
+describe('Server Tests', function() {
+
+  describe('API', function() {
+
+    it('should return array of all events when queried at /api/mobile/events', function(done) {
+      request(app)
+        .get('/api/mobile/events')
+        .end(function(err, res) {
+          var events = res.body;
+
+          assert.isArray(events);
+
+          assert.isObject(events[0])
+          assert.isArray(events[0].regions);
+          assert.isString(events[0].eventName);
+
+          done();
+        });
+    });
+  });
+
+  describe('Auth', function() {
+    
   });
 });
