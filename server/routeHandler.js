adff1d761e1e0723205e6d226c4c860e77646a4f
noodle <noodle_292@yahoo.com>
2014-12-30 19:26:25 -0800

    (feat) Create static files and serve them.  Closes #36

--- server/routeHandler.js
+++ server/routeHandler.js
@@ -1,17 +1,24 @@
 var express = require('express');
 var routeHandler = express.Router();
+var path = require('path');
 
-var mobileRouter = require('./routes/mobile/mobileRouter');
+var mobileApiRouter = require('./routes/mobile/mobileApiRouter');
 // var webAPIRouter = require('./routes/web/webAPIRouter');
 // var webRouter = require('./routes/web/webRouter');
 
-routeHandler.use('/api/mobile', mobileRouter);
+routeHandler.use('/api/mobile', mobileApiRouter);
 // routeHandler.use('/api/web', webAPIRouter);
 // routeHandler.use('*', webRouter);
 
 
 
-//Serve static files
-//router.use(express.static(path.resolve(__dirname + '/client')));
+//serve signup page as default
+routeHandler.get('/', function(req, res) {
+  res.sendFile(path.resolve(__dirname + '/../client/signup.html'));
+});
+
+//serve all static files
+routeHandler.use(express.static(__dirname + './../client'));
+
 
 module.exports = routeHandler;
