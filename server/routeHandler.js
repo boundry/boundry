363b0f1a6cb78e382f771ca76a6e21c4d8e4479a
Caroline Wong <carowong5@gmail.com>
2014-12-30 19:26:38 -0800

    (feat) adding signup and login methods to authRouter. Changed routes in routeHandler

--- server/routeHandler.js
+++ server/routeHandler.js
@@ -2,10 +2,14 @@ var express = require('express');
 var routeHandler = express.Router();
 
 var mobileRouter = require('./routes/mobile/mobileRouter');
-var webRouter = require('./routes/web/webRouter');
+// var webAPIRouter = require('./routes/web/webAPIRouter');
+// var webRouter = require('./routes/web/webRouter');
 
 routeHandler.use('/api/mobile', mobileRouter);
-routeHandler.use('/api/web', webRouter);
+// routeHandler.use('/api/web', webAPIRouter);
+// routeHandler.use('*', webRouter);
+
+
 
 //Serve static files
 //router.use(express.static(path.resolve(__dirname + '/client')));
