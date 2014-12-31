c071823812d839d14a0601a88001bae9b8a67d82
Caroline Wong <caro5@users.noreply.github.com>
2014-12-31 09:44:49 -0800

    Revert "(feat) Create static files and serve them.  Closes #36"

--- server/routeHandler.js
+++ server/routeHandler.js
@@ -1,24 +1,17 @@
 var express = require('express');
 var routeHandler = express.Router();
-var path = require('path');
 
-var mobileApiRouter = require('./routes/mobile/mobileApiRouter');
+var mobileRouter = require('./routes/mobile/mobileRouter');
 // var webAPIRouter = require('./routes/web/webAPIRouter');
 // var webRouter = require('./routes/web/webRouter');
 
-routeHandler.use('/api/mobile', mobileApiRouter);
+routeHandler.use('/api/mobile', mobileRouter);
 // routeHandler.use('/api/web', webAPIRouter);
 // routeHandler.use('*', webRouter);
 
 
 
-//serve signup page as default
-routeHandler.get('/', function(req, res) {
-  res.sendFile(path.resolve(__dirname + '/../client/signup.html'));
-});
-
-//serve all static files
-routeHandler.use(express.static(__dirname + './../client'));
-
+//Serve static files
+//router.use(express.static(path.resolve(__dirname + '/client')));
 
 module.exports = routeHandler;
