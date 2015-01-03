7fecdc5e09c9f17275181fb23743fa89bd943254
Grant Wu <grantwu2@gmail.com>
2015-01-03 14:22:41 -0800

    (feat) Scaffold Angular architecture
    Refactors index.html to Angular format, creates sensible client-side folder
    structure and begins migration of Caroline's initial gMaps code to Angular
    controllers and factories.

--- client/app/eventEditor/eventEditorService.js
+++ client/app/eventEditor/eventEditorService.js
@@ -1,12 +1,12 @@
 angular
-  .module('boundry.eventEditor', [])
-  .controller('eventEditorCtrl', eventEditorCtrl);
+  .module('boundry.eventEditor')
+  .factory('eventEditorService', eventEditorService);
+  
+  eventEditorService.$inject = ['$http'];
 
-  eventEditorCtrl.$inject = ['$scope', '$http'];
+  function eventEditorService ($http) {
 
-  function eventEditorCtrl ($scope, $http) {
-
-    $scope.mapVar = {
+    var mapVar = {
       drawingManager: new google.maps.drawing.DrawingManager({
         //drawingMode: the one that is selected
         drawingMode: google.maps.drawing.OverlayType.POLYGON,
@@ -28,36 +28,68 @@ angular
       boundaries: []
     };
 
+    var mapOptions = {
+      center: new google.maps.LatLng(37.789174, -122.419292),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var currAddress;
+
+    var export = {
+      getAddresses: getAddresses,
+      renderMap: renderMap
+    };
+    return export;
+
+
     //TODO: isolate address retrieval functionality and separate from rendering functionality
-    $scope.getAddress = function() {
+    function getAddresses() {
       var address = $('#addressInput').val(); // Get address from input
       var geocoder = new google.maps.Geocoder(); //Instantiate geocoder
-
-      var mapOptions = {
-        center: new google.maps.LatLng(37.789174, -122.419292),
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
 
       var map = new google.maps.Map($('#map')[0], mapOptions); //Create a map and pass it our 'map' html element
 
       geocoder.geocode( { 'address': address}, function(results, status) { //Geocode the grabbed address
         if (status === google.maps.GeocoderStatus.OK) {
-          console.log(results);
-          map.setCenter(results[0].geometry.location); //Set the center of our map
-          $scope.mapVar.drawingManager.setMap(map); //Render the map
+          console.log("ADDRESS RESULTS", results);
+          return results;
         } else {
-          alert('Geocode was not successful for the following reason: ' + status);
+          alert('Geocode failed: ' + status);
         }
       });
     };
 
+      geocoder.geocode( { 'address': address}, function(results, status) { //Geocode the grabbed address
+        if (status === google.maps.GeocoderStatus.OK) {
+          console.log("ADDRESS RESULTS", results);
+          map.setCenter(results[0].geometry.location); //Set the center of our map
+          mapVar.drawingManager.setMap(map); //Render the map
+        } else {
+          alert('Geocode was not successful for the following reason: ' + status);
+        }
+      });
     //Largely redundant code. When called, it should look for an address data point
     //and if it hasn't been set it should default to something. getAddress() should
     //merely set the data point. 
     //An event listener should then listen for that data change and fire this
     //function.
-    $scope.setMap = function (lt, lg) {
+    function renderMap(lt, lg) {
+
+      var mapCanvas = $('#map')[0]; //Grab our html map element
+
+      var map = new google.maps.Map(mapCanvas, mapOptions); //Same as above
+
+      mapVar.drawingManager.setMap(map); //Render, same as above
+
+      google.maps.event.addListener(mapVar.drawingManager, 'overlaycomplete', function(event){
+        // vertexListener(event.overlay);
+        //Grab path and push to array in mapVar
+        mapVar.boundaries.push(event.overlay.getPath().getArray());
+        $('#regionList').append('<div>'+ event.overlay.getPath().getArray() +'</div>'); //Append path to DOM
+
+      });
+    }
       var lat = lt || 37.789174;
       var lng = lg || -122.419292;
 
@@ -71,24 +103,14 @@ angular
 
       var map = new google.maps.Map(mapCanvas, mapOptions); //Same as above
 
-      $scope.mapVar.drawingManager.setMap(map); //Render, same as above
+      mapVar.drawingManager.setMap(map); //Render, same as above
 
-      google.maps.event.addListener($scope.mapVar.drawingManager, 'overlaycomplete', function(event){
+      google.maps.event.addListener(mapVar.drawingManager, 'overlaycomplete', function(event){
         // vertexListener(event.overlay);
         //Grab path and push to array in mapVar
-        $scope.mapVar.boundaries.push(event.overlay.getPath().getArray());
+        mapVar.boundaries.push(event.overlay.getPath().getArray());
         $('#regionList').append('<div>'+ event.overlay.getPath().getArray() +'</div>'); //Append path to DOM
 
       });
-    }
 
-    //We should render a map ASAP, and then async get the address
-    $(document).ready(function() {
-      $scope.setMap(); //give it lt, lg from form later
-    });
-
-
-
-  };
-
-
+};
